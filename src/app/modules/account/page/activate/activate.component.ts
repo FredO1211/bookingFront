7e261aa4252f60eae11d82c29a8557d4bc056d01
radoslawfedor <radoslaw.fedor@gmail.com>
2022-10-11 21:12:06 +0200

    create button group component

--- src/app/modules/account/page/activate/activate.component.ts
+++ src/app/modules/account/page/activate/activate.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ButtonGroupConfig } from 'src/app/modules/shared/dto/config/button-group-config';
 
 @Component({
   selector: 'app-activate',
   templateUrl: './activate.component.html',
-  styleUrls: ['./activate.component.scss']
+  styleUrls: ['./activate.component.scss'],
 })
 export class ActivateComponent implements OnInit {
   toggle = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getButtonGroupConfig(): ButtonGroupConfig[] {
+    const configList: ButtonGroupConfig[] = [];
+    configList.push(new ButtonGroupConfig('primary', 'label1', 'event'));
+    configList.push(new ButtonGroupConfig('primary', 'label2', 'event'));
+    configList.push(new ButtonGroupConfig('primary', 'label3', 'event'));
+    configList.push(new ButtonGroupConfig('primary', 'label4', 'event'));
+
+    return configList;
   }
 }
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonGroupComponent } from './components/button_group/button-group/button-group.component';
+import { MatButtonModule } from '@angular/material/button';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   exports: [ButtonGroupComponent],
   declarations: [ButtonGroupComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, CoreModule],
 })
 export class SharedModule {}
