5eedd539cf363415590dfe12890000fda07fffbc
Radosław Fedor <radoslaw.fedor@softres.pl>
2022-11-28 21:53:54 +0100

    refactor

---
+++ src/app/modules/main/component/main-sidenav/main-sidenav.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'main-sidenav',
+  templateUrl: './main-sidenav.component.html',
+  styleUrls: ['./main-sidenav.component.scss'],
+})
+export class MainSidenavComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
