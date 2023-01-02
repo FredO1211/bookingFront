7c45ea0e2d92d7a90766ed4130245ce384a1038a
Radosław Fedor <radoslaw.fedor@softres.pl>
2023-01-02 22:52:15 +0100

    guests arrival overview section

---
+++ src/app/modules/main/module/home/components/guest-arrival-overview/guest-arrival-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'guest-arrival-overview',
+  templateUrl: './guest-arrival-overview.component.html',
+  styleUrls: ['./guest-arrival-overview.component.scss'],
+})
+export class GuestArrivalOverviewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/modules/main/module/home/home.module.ts
+++ src/app/modules/main/module/home/home.module.ts
@@ -8,11 +8,18 @@ import { MatCardModule } from '@angular/material/card';
 import { WeekOverviewComponent } from './components/week-overview/week-overview.component';
 import { MatIconModule } from '@angular/material/icon';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
+import { GuestArrivalOverviewComponent } from './components/guest-arrival-overview/guest-arrival-overview.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
-  declarations: [HomePageComponent, WeekOverviewComponent],
+  declarations: [
+    HomePageComponent,
+    WeekOverviewComponent,
+    GuestArrivalOverviewComponent,
+  ],
   imports: [
     CommonModule,
+    MatTabsModule,
     MatGridListModule,
     MatDatepickerModule,
     MatNativeDateModule,
