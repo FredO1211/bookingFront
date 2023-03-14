700bfd8effe2e139674f45a7fc0283fe11966378
catherin3e <nkatarzyna.ling@gmail.com>
2023-03-14 23:13:26 +0100

    create dev setup

--- src/app/modules/core/api/activate/activate.api.ts
+++ src/app/modules/core/api/activate/activate.api.ts
@@ -1,11 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AccountConfigurationDTO } from 'src/app/modules/account/page/activate/dto/account-configuration.dto';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ActivateApi {
+  private BASE_URL = `${environment.backend_host}/account`;
   constructor(private httpClient: HttpClient) {}
 
-  activate() {}
+  activateAccount(
+    accountConfiguration: AccountConfigurationDTO,
+    token: string
+  ) {
+    const api_url = `${this.BASE_URL}/activate`;
+    const params = new HttpParams().append('token', token);
+
+    return this.httpClient.put(api_url, accountConfiguration, {
+      params: params,
+    });
+  }
 }
